bd4ccb8369b309f488cf7342839e6f8e80409e73
fuzzy69 <zeewolf@neobee.net>
2021-01-25 19:11:11 +0100

    Added quick sort algorithm

--- algorithms/quicksort.d
+++ algorithms/quicksort.d
@@ -1,10 +1,35 @@
 module quicksort;
 
-int[] quickSort(int[] arr)
-{
-    // TODO: finish this
+import std.algorithm.mutation : swap;
 
-    return arr;
+///
+int partition(int[] arr, int leftIndex, int rightIndex)
+{
+    int i = leftIndex - 1;
+    const int pivot = arr[rightIndex];
+    for (int j = leftIndex; j < rightIndex; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[rightIndex]);
+
+    return i + 1;
+}
+
+///
+void quickSort(int[] arr, int leftIndex, int rightIndex)
+{
+    int pivotIndex = 0;
+    if (leftIndex < rightIndex)
+    {
+        pivotIndex = partition(arr, leftIndex, rightIndex);
+        quickSort(arr, leftIndex, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, rightIndex);
+    }
 }
 
 unittest
@@ -15,9 +40,10 @@ unittest
     import std.range : generate, takeExactly;
     import std.algorithm : isSorted;
 
-	int[] randomArray = generate!(() => uniform(0, 100)).takeExactly(10).array;
-    auto sortedArray = quickSort(randomArray.dup);
+    const int arrayLength = 10;
+	int[] randomArray = generate!(() => uniform(0, 100)).takeExactly(arrayLength).array;
     writeln(randomArray);
-    writeln(sortedArray);
-    assert(sortedArray.isSorted);
+    quickSort(randomArray, 0, arrayLength - 1);
+    writeln(randomArray);
+    assert(randomArray.isSorted);
 }
