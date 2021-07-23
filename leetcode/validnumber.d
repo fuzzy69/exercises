86113f8733ac33316779054553271a4725bac289
fuzzy69 <zeewolf@neobee.net>
2021-07-23 06:34:52 +0200

    Added valid number problem solution

---
+++ leetcode/validnumber.d
@@ -0,0 +1,102 @@
+module leetcode.validnumber;
+
+/*
+https://leetcode.com/problems/valid-number/
+
+65. Valid Number
+
+A valid number can be split up into these components (in order):
+
+A decimal number or an integer.
+(Optional) An 'e' or 'E', followed by an integer.
+A decimal number can be split up into these components (in order):
+
+(Optional) A sign character (either '+' or '-').
+One of the following formats:
+One or more digits, followed by a dot '.'.
+One or more digits, followed by a dot '.', followed by one or more digits.
+A dot '.', followed by one or more digits.
+An integer can be split up into these components (in order):
+
+(Optional) A sign character (either '+' or '-').
+One or more digits.
+For example, all the following are valid numbers: ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
+
+Given a string s, return true if s is a valid number.
+
+ 
+
+Example 1:
+
+Input: s = "0"
+Output: true
+Example 2:
+
+Input: s = "e"
+Output: false
+Example 3:
+
+Input: s = "."
+Output: false
+Example 4:
+
+Input: s = ".1"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 20
+s consists of only English letters (both uppercase and lowercase), digits (0-9), plus '+', minus '-', or dot '.'.
+*/
+
+import std.ascii : isDigit, isWhite;
+
+class Solution
+{
+    static bool isNumber(string s)
+    {
+        assert(1 <= s.length && s.length <= 20);
+        bool result = false;
+        ulong i = 0;
+        // Skip leading whitespace
+        while (i < s.length && s[i].isWhite)
+            ++i;
+        // Sign
+        if (i < s.length && (s[i] == '+' || s[i] == '-'))
+        {
+            ++i;
+            result = true;
+        }
+        while (i < s.length && s[i].isDigit)
+        {
+            ++i;
+            result = true;
+        }
+        // Fractional part
+        if (i < s.length && s[i] == '.')
+        {
+            ++i;
+            while (i < s.length && s[i].isDigit)
+            {
+                ++i;
+                result = true;
+            }
+        }
+        // Skip ending whitespace
+        while (i < s.length && s[i].isWhite)
+            ++i;
+
+        return result && i == s.length;
+    }
+}
+
+unittest
+{
+    assert(Solution.isNumber("0"));
+    assert(!Solution.isNumber("e"));
+    assert(!Solution.isNumber("."));
+    assert(Solution.isNumber(".1"));
+    assert(Solution.isNumber("-10.23"));
+    assert(!Solution.isNumber("--10.23"));
+}
